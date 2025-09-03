339a8ba982957188d86a0e4704fcf43dd39e78e0
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-03 23:04:01 +0100

    Design Database Schema (DDL)

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,14 +1,74 @@
 -- A script that designs the database schema
 
 -- User table
-create TABLE IF NOT EXISTS `users` (
- id BINARY(16) PRIMARY KEY,
- first_name VARCHAR(256) NOT NULL,
- last_name VARCHAR(256) NOT NULL,
- email VARCHAR(256) UNIQUE NOT NULL,
- password_hash VARCHAR(256) NOT NULL,
- phone_number VARCHAR(20) NULL,
- role ENUM('host', 'guest', 'admin') NOT NULL DEFAULT 'guest',
- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+create TABLE IF NOT EXISTS `User` (
+    user_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(256) NOT NULL,
+    last_name VARCHAR(256) NOT NULL,
+    email VARCHAR(256) UNIQUE NOT NULL,
+    password_hash VARCHAR(256) NOT NULL,
+    phone_number VARCHAR(20) NULL,
+    role ENUM('host', 'guest', 'admin') NOT NULL DEFAULT 'guest',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX(email)
+);
 
 -- Property table
+create TABLE IF NOT EXISTS`Property` (
+    property_id CHAR(36) PRIMARY KEY,
+    host_id CHAR(36) NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(256) NOT NULL,
+    pricepernight DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (host_id) REFERENCES User(user_id),
+);
+
+-- Booking table
+CREATE TABLE IF NOT EXISTS `Booking` (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id)
+);
+
+-- Payment table 
+CREATE TABLE IF NOT EXISTS `Payment` (
+    payment_id CHAR(36) PRIMARY KEY,
+    booking_id CHAR(36) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+);
+
+-- Review
+CREATE TABLE IF NOT EXISTS `Review` (
+    review_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    rating INT  NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Message
+CREATE TABLE IF NOT EXISTS `Message` (
+    message_id CHAR(36) PRIMARY KEY,
+    sender_id CHAR(36) NOT NULL,
+    recipient_id CHAR(36) NOT NULL,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (recipient_id) REFERENCES User(user_id)
+);
