37034d80440e95966809b74b0a478312ba0e8720
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-07 11:09:30 +0100

    Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,6 +1,22 @@
 -- A script using join queries to retrieve and manipulate data from DB
 
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM Booking b
+INNER JOIN User u
+    ON b.user_id = u.user_id;
+
 /* -- Inner Join
+
+
 SELECT booking_id, total_price, status, first_name, last_name FROM `Booking`
 INNER JOIN `users` ON
 Booking.user_id = users.user_id;
