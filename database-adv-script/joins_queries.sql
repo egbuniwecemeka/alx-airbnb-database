d54a4f90fb0a791f816e76ce086a5479b8e9493e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-07 11:21:08 +0100

    Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,31 +1,16 @@
 -- A script using join queries to retrieve and manipulate data from DB
 
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price,
-    b.status,
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    u.email
-FROM Booking b
-INNER JOIN User u
-    ON b.user_id = u.user_id;
-
-/* -- Inner Join
-
-
-SELECT booking_id, total_price, status, first_name, last_name FROM `Booking`
-INNER JOIN `users` ON
+-- Inner Join
+SELECT booking_id, total_price, status, first_name, last_name FROM Booking
+INNER JOIN users ON
 Booking.user_id = users.user_id;
- */
 
 -- Left Join
-/* SELECT * FROM `Property`
-LEFT JOIN `Review`
-ON Property.property_id = Review.property_id; */
+SELECT * FROM Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id;
+
+
 /* 
 -- Right Join
 SELECT * FROM `Booking`
