c3cfc838faa170e9205820e534cff6e8f3440923
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-08 21:48:33 +0100

    Practice Subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,15 @@
+-- An SQL script that uses correlated and non-correlated subqueries to retrieve data from a DB
+
+-- Find all properties where the average rating is greater than 4
+-- Outer subquery: This retrieves all properties
+-- Inner subquery: Groups properties by reviews and displays those greater than 4
+-- This is Non-correlated, because the inner query runs independently of the outer query,
+-- i.e it does not access the outer query columns directly inside the inner query
+SELECT p.property_id, p.name, p.description
+FROM Property AS p
+WHERE p.property_id IN (
+    SELECT r.property_id
+    FROM Review AS r
+    GROUP BY r.property_id
+    HAVING AVG(r.rating) > 4
+);
