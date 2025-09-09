8cb197abd14fd5a5c0a173d573100fd4ece0615a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-09 10:29:51 +0100

    Apply Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,8 @@
+-- An SQL script using aggregation and window functions to analyze data
+
+-- Finding the total number of bookings made by each user.
+-- Using the COUNT fuction and GROUP BY clause
+
+SELECT COUNT(*) FROM Booking AS b
+-- Grouping by a unique column eg primary key is not good
+GROUP BY b.user_id;
