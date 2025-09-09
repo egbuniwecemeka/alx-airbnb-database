d200072aed41e0c248266cc4a96695be1c171d91
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-09 02:33:38 +0100

    Practice Suqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -8,9 +8,25 @@
 -- i.e it does not access the outer query columns directly inside the inner query
 SELECT p.property_id, p.name, p.description
 FROM Property AS p
-WHERE p.property_id IN (
+WHERE p.property_id IN ( -- IN expects a single column list
     SELECT r.property_id
     FROM Review AS r
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4.0
 );
+
+-- Correlated subquery to find users who have made more than 3 bookings
+-- Correlated subquery is a query where the inner subquery is dependent on the outer subquery.
+    -- WHERE works before grouping → it cannot see results of COUNT(), SUM(), or other aggregates.
+    -- HAVING works after grouping → it can filter based on those aggregate values
+SELECT u.user_id, u.first_name, u.last_name
+FROM users AS u
+WHERE (
+    -- SELECT b.user_id
+    SELECT COUNT(*)
+    FROM Booking AS b
+    -- At this point, I got to understand the use of GROUP BY. As without GROUP BY,
+    -- it returns all the rows of the column
+    -- GROUP BY b.user_id
+    WHERE b.user_id = u.user_id
+) > 2;
