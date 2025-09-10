0ae2e03959290cedab8c6fda47fe40cdd6328576
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-10 09:22:56 +0100

    Optimize Complex Queries

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,5 @@
 -- A script that creates appropriate indexes for columns
+-- Indexes only help when MySQL has to search, filter, sort, or join.
 
 -- Indexing user_id column of users in the Booking table
 CREATE INDEX ind_usr_book ON Booking(user_id);
@@ -7,14 +8,14 @@ CREATE INDEX ind_usr_book ON Booking(user_id);
 CREATE INDEX ind_prop_rev ON Review(property_id);
 
 -- Composite index on users table
-EXPLAIN
+EXPLAIN ANALYZE
 SELECT u.first_name, u.last_name
 FROM users AS u
 WHERE u.first_name = 'Emmanuel';
 
 CREATE INDEX ind_fname_lname ON users(first_name, last_name);
 
-ANALYZE
+EXPLAIN ANALYZE
 SELECT u.first_name, u.last_name
 FROM users AS u
 WHERE u.first_name = 'Emmanuel';;
