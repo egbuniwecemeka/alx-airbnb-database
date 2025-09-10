e9f8e9f237301319870758c62a8a7de1986f57ac
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-10 13:17:53 +0100

    Optimize Complex Queries

--- database-adv-script/optimization_report.md
+++ database-adv-script/optimization_report.md
@@ -1,1 +1,3 @@
 # Optimize Complex Queries
+
+In the performance.sql file, an initial query that returns the values of Booking table, alonsidee its user, properties and payment details. This query was analyzed using EXPLAIN
--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,10 +2,8 @@
 
 EXPLAIN
 SELECT b.booking_id,
-    b.property_id
 FROM Booking AS b
     INNER JOIN users as u
     INNER JOIN property as p
 -- Only one WHERE can be used which is joined multiple ANDs
-WHERE b.user_id = u.user_id AND b.property_id = p.property_id
-;
+WHERE b.user_id = u.user_id AND b.property_id = p.property_id;
